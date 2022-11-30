974ea78becabee2fe9807abeb2bce34d37a4119a
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-11-30 13:41:21 +0000

    Print uppercase

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,16 +2,16 @@
 import random
 number = random.randint(-10000, 10000)
 if number >= 0:
-        digit = number % 10
-        print("Last digit of {} is ".format(number), end="")
-        if digit > 5:
-                print("{} and is greater than 5".format(digit))
-        elif digit == 0:
-                print("{} and is 0".format(digit))
-        else:
-                print("{} and is less than 6 and not 0".format(digit))
+    digit = number % 10
+    print("Last digit of {} is ".format(number), end="")
+    if digit > 5:
+        print("{} and is greater than 5".format(digit))
+    elif digit == 0:
+        print("{} and is 0".format(digit))
+    else:
+        print("{} and is less than 6 and not 0".format(digit))
 
 if number < 0:
-        digit = number % -10
-        print("Last digit of {} is ".format(number), end="")
-        print("{} and is less than 6 and not 0".format(digit))
+    digit = number % -10
+    print("Last digit of {} is ".format(number), end="")
+    print("{} and is less than 6 and not 0".format(digit))
