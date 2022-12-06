e8df543e97768458bc3eac586cbe98b4f2ab6c1f
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-12-06 11:34:45 +0000

    Replace without modifying old list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,6 +5,6 @@ def replace_in_list(my_list, idx, element):
     elif (idx > len(my_list) - 1):
         return (my_list)
     else:
-        new_list = my_list
-        new_list [idx] = element
+        new_list = my_list.copy()
+        new_list[idx] = element
         return (new_list)
