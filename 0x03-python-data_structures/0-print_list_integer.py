048262c5e2b69111569b150a0d3b4b21204a3c07
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-12-06 09:32:32 +0000

    Print integer list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for x in range(len(my_list)):
-        print("{}".format(my_list[x]))
+        for x in range(len(my_list)):
+                    print("{:d}".format(my_list[x]))
