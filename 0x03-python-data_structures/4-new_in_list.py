491d34cc314839b43fa0df77a4c5589d09a70180
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-12-06 11:51:43 +0000

    Replace without modifying old list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     if (idx < 0):
         return (my_list)
     elif (idx > len(my_list) - 1):
