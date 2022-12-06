f975c84488bebecfc1c41e2961c64b2aaf206b81
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-12-06 14:29:58 +0000

    Replace string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     mydict = {67: None, 99: None}
-    new_string=(my_string.translate(mydict))
+    new_string = (my_string.translate(mydict))
     return new_string
