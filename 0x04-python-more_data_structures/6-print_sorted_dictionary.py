1c02e0d0d4bbb6b2bd6375b3eada6c3e52f9ba86
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-12-07 14:49:25 +0000

    Sotred dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    new_dict = dict(sorted(a_dictionary.items()))
-    for key, value in new_dict.items():
-        print(key, ':', value)
+    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
