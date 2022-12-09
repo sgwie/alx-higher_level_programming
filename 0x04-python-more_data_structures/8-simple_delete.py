ba53b42d3a3ef8242e65bb24ad4bfa840a547972
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-12-09 11:20:27 +0000

    Delete key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for i in a_dictionary:
-        if i == key:
-            del a_dictionary[key]
-            return a_dictionary
-        else:
-            return a_dictionary
+    if i in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
